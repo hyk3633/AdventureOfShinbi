67d732260c672a4173fa41239119f4bc9c711ddb
HeoYoungGyo <gjdudry@naver.com>
2022-08-13 09:48:01 +0900

    프로젝트 생성

---
+++ Source/AdventureOfShinbi/AdventureOfShinbiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdventureOfShinbiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVENTUREOFSHINBI_API AAdventureOfShinbiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
